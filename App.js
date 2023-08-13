03dd41fc1f980160e19ca2e3eb0853e874d91d0c
jin <kyuj0810@gmail.com>
2023-08-13 20:29:34 +0900

    14.6 AppLoading

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect, useCallback } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        const fontsToLoad = [Ionicons.font];
+        const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+        console.log(fontPromises);
+        // Pre-load fonts, make any API calls you need to do here
+
+        //await Font.loadAsync(Entypo.font);
+        await new Promise.all(fontPromises);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setLoading(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (loading) {
+      await SplashScreen.hideAsync();
+    }
+  }, [loading]);
+
+  if (!loading) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text>Hello!!ㅋㅋㄱ</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ instaclone-native/instaclone-native/instaclone_nativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  instaclone_nativeApp.swift
+//  instaclone-native
+//
+//  Created by 김유진 on 2023/08/11.
+//
+
+import SwiftUI
+
+@main
+struct instaclone_nativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
