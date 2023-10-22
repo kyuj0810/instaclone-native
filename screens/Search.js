bf76582033b40e4391fe6601146a41ac21845d94
jin <kyuj0810@gmail.com>
2023-10-22 22:26:49 +0900

    Search part Three

--- screens/Search.js
+++ screens/Search.js
@@ -1,42 +1,85 @@
+import { gql, useLazyQuery } from '@apollo/client';
 import React, { useEffect } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { useForm } from 'react-hook-form';
+import { ActivityIndicator, Text, TextInput, View } from 'react-native';
 import styled from 'styled-components/native';
 import DismissKeyboard from '../components/DismissKeyboard';
-import { useForm } from 'react-hook-form';
+
+const SEARCH_PHOTOS = gql`
+  query searchPhotos($keyword: String!) {
+    searchPhotos(keyword: $keyword) {
+      id
+      file
+    }
+  }
+`;
+
+const MessageContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
+const MessageText = styled.Text`
+  margin-top: 15px;
+  color: white;
+  font-weight: 600;
+`;
 
 const Input = styled.TextInput``;
 
 export default function Search({ navigation }) {
-  const { setValue, register } = useForm();
+  const { setValue, register, watch, handleSubmit } = useForm();
+  const [startQueryFn, { loading, data, called }] = useLazyQuery(SEARCH_PHOTOS);
+  const onValid = ({ keyword }) => {
+    startQueryFn({
+      variables: {
+        keyword,
+      },
+    });
+  };
   const SearchBox = () => (
     <TextInput
-      style={{ backgroundColor: 'whte' }}
+      style={{ backgroundColor: 'white' }}
       placeholderTextColor="black"
-      placeholder="Search Photo"
+      placeholder="Search photos"
       autoCapitalize="none"
       returnKeyLabel="Search"
       returnKeyType="search"
       autoCorrect={false}
       onChangeText={(text) => setValue('keyword', text)}
+      onSubmitEditing={handleSubmit(onValid)}
     />
   );
   useEffect(() => {
     navigation.setOptions({
       headerTitle: SearchBox,
     });
-    register('keyword');
+    register('keyword', {
+      required: true,
+      minLength: 3,
+    });
   }, []);
+  console.log(data);
   return (
     <DismissKeyboard>
-      <View
-        style={{
-          backgroundColor: 'black',
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Text style={{ color: 'white' }}>Photo</Text>
+      <View style={{ flex: 1, backgroundColor: 'black' }}>
+        {loading ? (
+          <MessageContainer>
+            <ActivityIndicator size="large" />
+            <MessageText>Searching...</MessageText>
+          </MessageContainer>
+        ) : null}
+        {!called ? (
+          <MessageContainer>
+            <MessageText>Search by keyword</MessageText>
+          </MessageContainer>
+        ) : null}
+        {data?.searchPhotos !== undefined &&
+        data?.searchPhotos?.length === 0 ? (
+          <MessageContainer>
+            <MessageText>Could not find anything.</MessageText>
+          </MessageContainer>
+        ) : null}
       </View>
     </DismissKeyboard>
   );
